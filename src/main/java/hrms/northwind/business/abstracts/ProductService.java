c1a7a8b48dc5ef330bce2e3c305b81c520d61ed6
ermankaratash <48660429+ermankaratash@users.noreply.github.com>
2021-11-08 23:21:56 +0100

    Results added

--- src/main/java/hrms/northwind/business/abstracts/ProductService.java
+++ src/main/java/hrms/northwind/business/abstracts/ProductService.java
@@ -1,10 +1,15 @@
 package hrms.northwind.business.abstracts;
 
 import java.util.List;
+
+import hrms.northwind.core.utilities.results.DataResult;
+import hrms.northwind.core.utilities.results.Result;
 import hrms.northwind.entities.concretes.Product;
 
 public interface ProductService {
 	
-	List<Product> getAll();
+	DataResult<List<Product>> getAll();
+	
+	Result add(Product product);
 
 }
